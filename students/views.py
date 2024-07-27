7340aed1e8bd86abff575eee9266a141f645ef8a
arjun-guvi <arjun.a@guvi.in>
2024-07-27 17:12:15 +0530

    Update views.py

--- students/views.py
+++ students/views.py
@@ -8,7 +8,7 @@ def student_list(request):
     students = Student.objects.all()
     return render(request, 'students/student_list.html', {'students': students})
 
-def student_detail(request gfsjh, pk):
+def student_detail(request, pk):
     student = get_object_or_404(Student, pk=pk)
     return render(request, 'students/student_detail.html', {'student': student})
 
