98691763189ad71aa6464fb4d8a63bf5c6e69087
arjun-guvi <arjun.a@guvi.in>
2024-07-27 10:34:28 +0530

    Update urls.py

--- students/urls.py
+++ students/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('students/', views.student_list, name='student_list'),
     path('students/<int:pk>/', views.student_detail, name='student_detail'),
-    path('students/new/', views.student_create, name='student_create'),
+    path('new/', views.student_create, name='student_create'),
     path('students/<int:pk>/edit/', views.student_update, name='student_update'),
     path('students/<int:pk>/delete/', views.student_delete, name='student_delete'),
 ]
